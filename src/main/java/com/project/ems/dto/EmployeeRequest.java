fde72a54b7ec60ba8ee66e924bceea03cfc633b8
vishwajeet30 <vishwajeetsngh30@gmail.com>
2026-06-26 18:44:41 +0530

    created EmployeeRequest.java file in DTO added all the entities

--- src/main/java/com/project/ems/dto/EmployeeRequest.java
+++ src/main/java/com/project/ems/dto/EmployeeRequest.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public record EmployeeRequest (
     @NotBlank(message = "Employee code is required")
