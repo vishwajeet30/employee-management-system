7c38d8a863831224a40001425e4d72a6de145987
vishwajeet30 <vishwajeetsngh30@gmail.com>
2026-06-26 17:56:04 +0530

    save Employees.java

---
+++ src/main/java/com/project/ems/entity/Employee.java
@@ -0,0 +1,45 @@
+package com.project.ems.entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String employeeCode;
+
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+    private String department;
+    private String designation;
+    @Column(nullable = false)
+    private BigDecimal salary;
+    private String joiningDate;
+    private Boolean status = true;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = LocalDateTime.now();
+    }
+
+
+}
