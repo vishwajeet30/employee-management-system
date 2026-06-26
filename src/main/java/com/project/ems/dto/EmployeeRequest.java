90d6279a8094e41fbf7bb5adca07b113c2b88241
vishwajeet30 <vishwajeetsngh30@gmail.com>
2026-06-26 18:44:16 +0530

    created EmployeeRequest.java file in DTO added all the entities

--- src/main/java/com/project/ems/dto/EmployeeRequest.java
+++ src/main/java/com/project/ems/dto/EmployeeRequest.java
@@ -1,4 +1,30 @@
 package com.project.ems.dto;
 
-public class EmployeeRequest {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import java.math.BigDecimal;
+
+public record EmployeeRequest (
+    @NotBlank(message = "Employee code is required")
+    String employeeCode,
+    @NotBlank(message = "First Name is required")
+    String firstName,
+    @NotBlank(message = "Last Name is required")
+    String lastName,
+    @Email
+    @NotBlank
+    String email,
+    String phone,
+    String departmemnt,
+    String designation,
+
+    @NotNull
+    @Positive
+    BigDecimal salary,
+    @NotNull
+    LocalDate joiningDate
+
+    ) {}
